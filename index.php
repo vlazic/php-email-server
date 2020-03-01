70fe79984b52787b208149aef7c420a2e4649726
Vladimir Lazić <vlazic.work@gmail.com>
2020-03-01 23:45:54 +0100

    Added Sabre HTTP librarary

--- index.php
+++ index.php
@@ -1,91 +1,32 @@
 <?php
+use Sabre\HTTP;
+
 require_once 'vendor/autoload.php';
 
-header("Access-Control-Allow-Origin: *");
-header('Content-Type: application/json');
 
-// fix for javascript fetch
-// https://stackoverflow.com/questions/36669911/post-not-retrieving-data-from-javascripts-fetch
-if (count($_POST) === 1) {
-    $_POST = json_decode(file_get_contents('php://input'), true);
+
+// create request and response instances
+$request = HTTP\Sapi::getRequest();
+$response = new HTTP\Response();
+
+// set default headers
+$response->setHeader('Access-Control-Allow-Origin', '*');
+$response->setHeader('Content-Type', 'application/json');
+
+// parse .env file
+$data = parse_env($request, $response);
+
+// show errors only if DEBUG env var is set to TRUE
+if ($_ENV['DEBUG']) {
+    $whoops = new \Whoops\Run;
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    $whoops->register();
+} else {
+    error_reporting(0);
 }
 
+// parse HTTP request
+$data = parse_request($request, $response, $data);
 
-function return_error($str)
-{
-    http_response_code(400);
-    die(json_encode(['error'=>$str]));
-}
-function return_success($str)
-{
-    http_response_code(200);
-    die(json_encode(['success'=>$str]));
-}
-
-// load .env variables
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-$dotenv->required([
-    "TOKEN",
-    "FROM_EMAIL",
-    "FROM_NAME",
-    "SMTP_USER",
-    "SMTP_PASSWORD",
-    "SMTP_SERVER",
-    "SMTP_PORT"
-    ])->notEmpty();
-    
-$token = $_ENV["TOKEN"];
-$from_email = $_ENV["FROM_EMAIL"];
-$from_name = $_ENV["FROM_NAME"];
-$smtp_user = $_ENV["SMTP_USER"];
-$smtp_password = $_ENV["SMTP_PASSWORD"];
-$smtp_server = $_ENV["SMTP_SERVER"];
-$smtp_port = $_ENV["SMTP_PORT"];
-
-// parse POST arguments
-$required_post = ['subject','to','message', 'token'];
-foreach ($required_post as $field) {
-    if (!isset($_POST[$field])) {
-        return_error("Field '${field}' is missing");
-    }
-    if (empty($_POST[$field])) {
-        return_error("Field '${field}' is empty");
-    }
-}
-
-// validate token
-if ($token !== $_POST['token']) {
-    return_error("Your token is invalid");
-}
-
-$subject = $_POST['subject'];
-$to = isset($_POST['to_name']) && !empty($_POST['to_name']) ?
-    [$_POST['to']=>$_POST['to_name']] :
-    [$_POST['to']];
-$from = isset($_POST['from_name']) && !empty($_POST['from_name']) ?
-    [$from_email => $_POST['from_name'] ]:
-    [$from_email => $from_name];
-$message = $_POST['message'];
-    
-    // Create the Transport
-$transport = (new Swift_SmtpTransport($smtp_server, $smtp_port))
-  ->setUsername($smtp_user)
-  ->setPassword($smtp_password)
-;
-
-// Create the Mailer using your created Transport
-$mailer = new Swift_Mailer($transport);
-
-// Create a message
-$message = (new Swift_Message($subject))
-  ->setFrom($from)
-  ->setTo($to)
-  ->setBody($message)
-  ;
-
-// Send the message
-$result = $mailer->send($message);
-
-
-return_success(true);
+// send email
+send_email($request, $response, $data);
